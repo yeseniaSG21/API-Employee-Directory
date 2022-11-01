6e474ae1ece5afda5d05790d63a1e20da2a48b32
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-31 23:12:31 -0700

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,43 @@ let employees = [];
 //Stores the DOM element that is the container for the employees
 const galleryContainer = document.getElementById('gallery');
 
-//Fetch data from the API
+/*****
+  Fetch data from the API
+*****/
 fetch('https://randomuser.me/api/?results=12&nat=US')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(response => response.results)
+  .then(displayEmployees)
+  .catch(err => console.log(err))
+
+/*****
+  Function displayEmployees will create the HTML markup for the employee elements.
+  It will loop through each of the 12 employees to add each to the HTML through a template literal.
+*****/
+function displayEmployees(employeeData) {
+  employees = employeeData;
+  let employeeHTML = '';
+
+  employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+
+    employeeHTML += `
+      <div class="card" data-index="${index}">
+        <div class="card-img-container">
+            <img class="card-img" src="${picture.large}">
+        </div>
+        <div class="card-info-container">
+          <h3 id="name" class="card-name cap">${name.first} ${name.last}</h3>
+          <p class="card-text">${email}</p>
+          <p class="card-text cap">${city}</p>
+        </div>
+      </div>
+    `
+  });
+
+  gallery.insertAdjacentHTML('beforeend', employeeHTML);
+
+}
