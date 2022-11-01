e353303e646efc3f91176258cb1dd7df925f999c
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-31 21:51:30 -0700

    Update

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//Empty array that will hold values from the API
+let employees = [];
+
+//Stores the DOM element that is the container for the employees
+const galleryContainer = document.getElementById('gallery');
