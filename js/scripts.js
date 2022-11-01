4fb57d025c2e7a0ea4ae32b17ba7e0b7b86b0b66
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-31 22:17:59 -0700

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ let employees = [];
 
 //Stores the DOM element that is the container for the employees
 const galleryContainer = document.getElementById('gallery');
+
+//Fetch data from the API
+fetch('https://randomuser.me/api/?results=12&nat=US')
+  .then(response => response.json())
+  .then(json => console.log(json))
